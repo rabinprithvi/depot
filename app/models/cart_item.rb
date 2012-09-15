80234a91eeeef4637c8e4591e2d0e9b5a1eea37b
rabinprithvi <rabindranath.prithvi@gmail.com>
2012-09-15 15:58:33 +0530

    Iteration C3 done

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -6,22 +6,25 @@
 # We make no guarantees that this code is fit for any purpose. 
 # Visit http://www.pragmaticprogrammer.com/titles/rails3 for more book information.
 #---
-class Cart
-  attr_reader :items   
+class CartItem
+
+  attr_reader :product, :quantity
   
-  def initialize
-    @items = []
+  def initialize(product)
+    @product = product
+    @quantity = 1
   end
   
-  
-  def add_product(product)
-    current_item = @items.find {|item| item.product == product}
-    if current_item
-      current_item.increment_quantity
-    else
-      @items << CartItem.new(product)
-    end
+  def increment_quantity
+    @quantity += 1
   end
   
+  def title
+    @product.title
+  end
+  
+  def price
+    @product.price * @quantity
+  end
 end
-			
+
